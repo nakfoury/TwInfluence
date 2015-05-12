11a6da4b1bf39d3db1095e09c4959e94c2da2d87
nakfoury <nakfoury@gmail.com>
2015-05-12 17:05:32 -0500

    Still trying to fix file organization

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -10,9 +11,9 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/* Put OAuth stuff here */
+//blahblah
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    //
+    // res.render('fubar', { title: 'Zombo.com' });
+    res.send('TwInfluence');
+});
+
+router.get('nomatter', function(req, res, next) {
+    var query = 'aaaaa'; //it'll come from the req
+    var result = {};
+    //use twitter to update result
+    res.json(result);
 });
 
 module.exports = router;
