3a542a77b54ad54cba845077dfc49578e4da1231
nakfoury <nakfoury@gmail.com>
2015-05-13 17:42:49 -0500

    Spits out tweets lol

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var T = new Twit({
     consumer_key: 'luzRJQdzj4g0L7mjTZRDpo0PO',
     consumer_secret: 'ABYUGC4a5qA5NCaV6VH5zO3Y3u9RhdGU5IeNiEHdXmz4fdWS34',
     access_token: '392365525-X2Dh0aOkfp1EQt2R9o2bX17PkW60lmxVKEyTdr22',
-    access_token_secret: 'fFWofKgtDPybsipGkwFSiZWqFb0pVYE59qr6TbLiBhHFY',
+    access_token_secret: 'fFWofKgtDPybsipGkwFSiZWqFb0pVYE59qr6TbLiBhHFY'
     //callback: '52.24.28.184:3000/index.html'
 });
 
@@ -17,20 +17,15 @@ router.get('/', function(req, res, next) {
     //res.send('TwInfluence');
 });
 
-//router.post('/button', function(req, res) {
-//    data = req.body;
-//
-//}
-
-
-router.get('nomatter', function(req, res, next) {
-    var query = 'banana'; //it'll come from the req
+router.post('/hashtag', function(req, res) {
+    console.log(req.body);
+    var query = req.body['querytext'];
     var result = {};
     T.get('search/tweets', {q: query, count: 100}, function(err, data, response) {
         console.log(data);
         result = data;
+        res.json(result);
     });
-    res.json(result);
 });
 
 module.exports = router;
