3309df8c93195af94d2b68743b72516c199899b3
nakfoury <nakfoury@gmail.com>
2015-05-27 19:12:06 -0500

    Disabled retweeter sort (temporary)

--- routes/index.js
+++ routes/index.js
@@ -109,7 +109,7 @@ router.post('/hashtag', function(req, res) {
             result.children[l].tweet = tweet.text;
             result.children[l].retweetNo = tweet.retweet_count;
             //loadRetweeters(tweet.id_str, result, i);
-            T.get('statuses/retweeters/ids', {id: tweet.id_str, count: 15, stringify_ids: true}, function(err, data2, response) {
+            T.get('statuses/retweeters/ids', {id: tweet.id_str, count: 5, stringify_ids: true}, function(err, data2, response) {
                 if (err) {
                     console.log(err);
                     console.log("at retweeter list query (statuses/retweeters/ids)");
@@ -128,10 +128,10 @@ router.post('/hashtag', function(req, res) {
                                 console.log("at retweeter lookup (users/show");
                             }
                             else {
-                                //result.children[k].children[j].name = data3.screen_name;
-                                //result.children[k].children[j].followerNo = data3.followers_count;
-                                //result.children[k].children[j].image = data3.profile_image_url;
-                                sortTopRetweeters(k, data3)
+                                result.children[k].children[j].name = data3.screen_name;
+                                result.children[k].children[j].followerNo = data3.followers_count;
+                                result.children[k].children[j].image = data3.profile_image_url;
+                                //sortTopRetweeters(k, data3)
                             }
                             jf.writeFileSync("public/twitter_data.json", result);
                             j++;
