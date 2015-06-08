e526a2356eec56c1e19ad0551b1c6cad97339a23
nakfoury <nakfoury@gmail.com>
2015-06-08 00:25:44 -0500

    File location for deployment

--- routes/index.js
+++ routes/index.js
@@ -230,7 +230,7 @@ var buildJSON = function(query) {
                                 }
                             });
                             j++;
-                            jf.writeFileSync("public/twitter_data.json", result);
+                            jf.writeFileSync("/home/ec2-user/TwInfluence/public/twitter_data.json", result);
                         }
                     });
                 }
