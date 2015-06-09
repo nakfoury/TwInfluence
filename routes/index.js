078aebb8a573ed620386425fdf8da47fc23bca3d
nakfoury <nakfoury@gmail.com>
2015-06-08 20:54:01 -0500

    New master

--- routes/index.js
+++ routes/index.js
@@ -233,7 +233,7 @@ var buildJSON = function(query) {
                                 }
                             });
                             j++;
-                            jf.writeFileSync("public/twitter_data.json", result);
+                            jf.writeFileSync("public/twitter_data.json", result); // May need to be modified for deployment
                         }
                     });
                 }
